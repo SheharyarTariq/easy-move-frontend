4cdfecf945851030c2b0f128bbff133e138ae849
SheharyarTariq <sheharyartariqbutt@gmail.com>
2025-09-20 02:30:50 +0500

    not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,23 @@
-'use client'
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 
 const NotFound = () => {
-  const path = usePathname();
-
-  useEffect(() => {
-    console.error(
-      "404 Error: User attempted to access non-existent route:",
-      path
-    );
-  }, [path]);
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <Link href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+    <section className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="text-center px-6 py-12 max-w-md rounded-3xl bg-card/95 border border-border/40 shadow-xl shadow-accent/20 backdrop-blur-sm">
+        <h1 className="text-6xl font-extrabold text-primary mb-4">404</h1>
+        <p className="text-xl text-primary/90 mb-8">
+          Oops! The page you&apos;re looking for doesn&apos;t exist.
+        </p>
+        <Link href="/" className="flex items-center justify-center">
+          <Button className="hidden md:flex bg-cta-gradient hover:cursor-pointer hover:opacity-90 shadow-button-custom">  
+            Return to Home
+            <ArrowRight className="ml-2 h-5 w-5" />
+          </Button>
         </Link>
       </div>
-    </div>
+    </section>
   );
 };
 
